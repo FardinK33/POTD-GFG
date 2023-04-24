7aee28d7a9f2d7d2af9c0a4b36924917fdb260e6
Fardin Khan <112811643+FardinK33@users.noreply.github.com>
2023-04-24 15:35:54 +0530

    23-Apr-2023

--- 27_Minimum_Number.cpp
+++ 27_Minimum_Number.cpp
@@ -3,7 +3,7 @@ public:
     int minimumNumber(int n,vector<int> &arr){
         
         int min=INT_MAX;
-        
+         
         for(int i=0; i<n; i++){
             if( arr[i] & 1 ){
                 return 1;
